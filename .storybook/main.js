c853883120ab45cc9ba60b8ca599234506fd0a5c
shakti <shaktisaxena82@gmail.com>
2022-01-05 15:35:37 +0100

    Gitub action Setup

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,8 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+stories: ['../src/app/components/**/*.stories.ts'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
   ],
 };
